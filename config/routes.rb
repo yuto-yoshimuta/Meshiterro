6bfa8f19fb3ee28f929f729366a3dce27eca413c
yuto-yoshimuta <yuuto819midorimatu@gmail.com>
2024-08-05 08:45:13 +0000

    [Finish]9

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :post_images, only: [:new, :index, :show]
   devise_for :users
   root to: "homes#top"
   get 'homes/about', to: 'homes#about', as: 'about'
