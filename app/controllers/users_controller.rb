630dc85ecd3050d24927eedda6711b9fc3320b60
yuto-yoshimuta <yuuto819midorimatu@gmail.com>
2024-08-06 05:28:13 +0000

    [Finish]15

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,15 @@
+class UsersController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+    @post_images = @user.post_images
+  end
+
+  def edit
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :profile_image)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "homes#top"
   get 'homes/about', to: 'homes#about', as: 'about'
-
   resources :post_images, only: [:new, :create, :index, :show, :destroy]
+  resources :users, only: [:show, :edit]
 end
