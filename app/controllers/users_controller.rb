f243c4ad1ebd2e8f54c76af458960ad14b9b8d55
yuto-yoshimuta <yuuto819midorimatu@gmail.com>
2024-08-08 08:50:33 +0000

    [Finish]

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :is_matching_login_user, only: [:edit, :update]
+
   def show
     @user = User.find(params[:id])
     @post_images = @user.post_images.page(params[:page])
@@ -8,11 +10,11 @@ class UsersController < ApplicationController
   def edit
     @user = User.find(params[:id])
   end
-
+    
   def update
     @user = User.find(params[:id])
     @user.update(user_params)
-    redirect_to user_path(@user)
+    redirect_to user_path(@user.id)
   end
 
 
@@ -20,5 +22,12 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:name, :profile_image)
+  end
+
+  def is_matching_login_user
+    user = User.find(params[:id])
+    unless user.id == current_user.id
+      redirect_to post_images_path
+    end
   end
 end
